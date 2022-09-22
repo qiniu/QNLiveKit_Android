aa0fc02e9315a2c48ad249c1cb1847d4f3dd9d58
manjiale <1013666753@qq.com>
2022-09-22 10:47:17 +0800

    【优化】优化下拉刷新上拉加载效果

--- liveroom-uikit-core/src/main/java/com/qlive/uikitcore/refresh/ILoadView.kt
+++ liveroom-uikit-core/src/main/java/com/qlive/uikitcore/refresh/ILoadView.kt
@@ -2,19 +2,28 @@ package com.qlive.uikitcore.refresh
 
 import android.content.Context
 import android.view.View
+import com.qlive.uikitcore.R
 import com.qlive.uikitcore.refresh.QRefreshLayout.OnRefreshListener
 
-abstract class ILoadView(val mContext: Context, val mParent: QRefreshLayout) {
+abstract class ILoadView(val context: Context) {
+    var isShowLoadMore = false
+        private set
+    var isShowLoading = false
+        private set
+    var noMoreText = context.getText(R.string.no_more_tips)
 
-    var currentHeight: Int = 0
-    var isLoading: Boolean = false
-    abstract fun clear()
-    abstract fun getAttachView(): View
-    abstract val defaultHeight: Int
-    abstract fun onPointMove(height: Int): Int
-    abstract fun onPointUp(totalDistance: Float): Int
-    abstract fun setRefreshListener(mListener: OnRefreshListener?)
     abstract fun checkHideNoMore()
-    abstract fun finishLoadMore(isNoMore: Boolean)
+    abstract fun getFreshHeight(): Int
+    abstract fun maxScrollHeight(): Int
+    abstract fun getAttachView(): View
 
+    abstract fun onPointMove(totalY: Float, dy: Float)
+    open fun onPointUp(toStartLoad: Boolean) {
+        isShowLoading = toStartLoad
+    }
+
+    open fun onFinishLoad(showNoMore: Boolean) {
+        isShowLoading = false
+        isShowLoadMore = showNoMore
+    }
 }
---
+++ liveroom-uikit-core/src/main/java/com/qlive/uikitcore/refresh/IRefreshView.kt
@@ -0,0 +1,20 @@
+package com.qlive.uikitcore.refresh
+
+import android.animation.ObjectAnimator
+import android.content.Context
+import android.view.View
+import com.qlive.uikitcore.refresh.QRefreshLayout.OnRefreshListener
+
+abstract class IRefreshView(val context: Context) {
+
+    internal var recoverAnimator: ObjectAnimator? = null
+    abstract fun getFreshTopHeight():Int
+    abstract fun getFreshHeight(): Int
+    abstract fun maxScrollHeight(): Int
+    abstract fun getAttachView(): View
+    abstract fun isFloat(): Boolean
+
+    abstract fun onPointMove(totalY: Float, dy: Float)
+    abstract fun onPointUp(toStartRefresh: Boolean)
+    abstract fun onFinishRefresh()
+}
